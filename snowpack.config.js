460d10bb5e4fc003b7646750894adb99285a76af
Vjacheslav Borisov <slavb@ilb.ru>
2021-02-20 11:29:01 +0400

    initial snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,21 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    web: '/'
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  }
+};
