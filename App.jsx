368a638db8b85c65554eecf5f513572f39bfc29b
Vjacheslav Borisov <slavb@ilb.ru>
2021-04-23 11:05:26 +0400

    test calendar

--- App.jsx
+++ App.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import 'semantic-ui-offline/semantic.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import AppForm from './AppForm';
+import ManualForm from './ManualForm';
+import { Segment } from 'semantic-ui-react';
 
 export default function App() {
   return (
     <div className="ui container">
       <h1>Hello World!</h1>
-      <AppForm />
+      <Segment>
+        <AppForm />
+      </Segment>
+      <Segment>
+        <ManualForm />
+      </Segment>
     </div>
   );
 }
---
+++ ManualForm.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AutoField, AutoForm, SubmitField } from 'uniforms-semantic';
+import { createSchemaBridge } from './src';
+import { CustomAutoField } from './src';
+
+export default function AppForm() {
+  function onSubmit(data) {
+    alert(JSON.stringify(data));
+  }
+  const schema = {
+    title: 'Guest',
+    type: 'object',
+    properties: {
+      firstName: { type: 'string', minLength: 1 },
+      lastName: { type: 'string', minLength: 1 },
+      begDate: { type: 'string', format: 'date' },
+      endDate: { type: 'string', format: 'date' }
+    }
+  };
+
+  const model = {
+    firstName: 'John',
+    lastName: 'Unknown',
+    begDate: '2021-04-15'
+  };
+
+  return (
+    <AutoForm
+      schema={createSchemaBridge(schema)}
+      model={model}
+      onSubmit={onSubmit}
+      autoField={CustomAutoField}
+      showInlineError={true}>
+      <AutoField name="firstName" />
+      <AutoField name="lastName" />
+
+      <CustomAutoField name="begDate" />
+      <CustomAutoField name="endDate" />
+
+      <SubmitField value="Сохранить" />
+    </AutoForm>
+  );
+}
