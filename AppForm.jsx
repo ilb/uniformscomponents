bd194259af663c351b82ca83e835327855917cf4
Chikurov Pavel <84593835+CheThePavle@users.noreply.github.com>
2021-05-31 13:26:38 +0400

    Update AppForm.jsx

--- AppForm.jsx
+++ AppForm.jsx
@@ -49,7 +49,7 @@ export default function AppForm() {
         type: 'number',
         uniforms: { decimalScale: 0, thousandSeparator: null, format: '####' }
       },
-      regDate: { type: 'string', format: 'date', displayType: 'text' },
+      regDate: { type: 'string', format: 'date' },
       workExperience: {
         description: 'Work experience in years',
         type: 'integer',
