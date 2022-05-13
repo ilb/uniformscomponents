c7d7039c7fc7ca0976f85e293d4dec0aaa4a260e
ssortia <alexkiyan.lug@gmail.com>
2022-05-13 08:31:59 +0300

    added displayType text for contact list

--- src/ContactListField.jsx
+++ src/ContactListField.jsx
@@ -1,18 +1,50 @@
 import { connectField } from 'uniforms';
 import { ListField, ListItemField } from 'uniforms-semantic';
 import { CustomAutoField, PhoneField } from './index';
+import TextField from './TextField';
 
 function ContactListField(props) {
+  const displayType = props.field.uniforms?.displayType || 'input';
+
   return (
-    <ListField>
-      <ListItemField name="$">
-        {props.items.properties &&
-          Object.keys(props.items.properties).map((item) => (
-            <CustomAutoField name={item} key={item} />
-          ))}
-        {!props.items.properties && <PhoneField label={null} />}
-      </ListItemField>
-    </ListField>
+    <>
+      {displayType === 'input' && (
+        <ListField>
+          <ListItemField name="$">
+            {props.items.properties &&
+              Object.keys(props.items.properties).map((item) => (
+                <CustomAutoField name={item} key={item} />
+              ))}
+            {!props.items.properties && <PhoneField label={null} />}
+          </ListItemField>
+        </ListField>
+      )}
+      {displayType === 'text' && (
+        <div>
+          <div className="contacts-label-wrapper">
+            <label className="contacts-label">{props.label}</label>
+          </div>
+          {props.items.properties && (
+            <div className="contacts-object">
+              {props.value.map((object, i) => (
+                <>
+                  {Object.keys(props.items.properties).map((item) => (
+                    <TextField key={i} name={i + '.' + item} displayType="text" />
+                  ))}
+                </>
+              ))}
+            </div>
+          )}
+          {!props.items.properties && (
+            <div className="contacts-array">
+              {props.value.map((value, i) => (
+                <TextField key={i} label={null} value={value} name={i} displayType="text" />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 }
 
