f69856a72f2506c6943c7e2a4aad1c7474557559
ssortia <alexkiyan.lug@gmail.com>
2022-08-08 16:18:17 +0300

    added "latin" and "cyrillic" keywords to ajv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import Ajv from 'ajv';
 import JSONSchemaBridge from 'uniforms-bridge-json-schema';
 import localize from 'ajv-i18n';
-import { checkInn } from './utils/rules';
-//import addFormats from 'ajv-formats';
+import registerAjvKeywords from './utils/registerAjvKeywords';
 export { default as CustomAutoField } from './CustomAutoField';
 export { default as DateField } from './DateField';
 export { default as PhoneField } from './PhoneField';
@@ -16,76 +15,10 @@ ajv.addKeyword('options');
 // email or empty string
 ajv.addFormat(
   'email',
-  /(^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$)|(^$)/
+  /(^[a-z\d.!#$%&'*+/=?^_`{|}~-]+@[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?(?:\.[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?)*$)|(^$)/
 );
 
-ajv.addKeyword('isNotEmpty', {
-  type: 'string',
-  errors: true,
-  validate: function validate(schema, data, parent, key) {
-    validate.errors = [
-      {
-        keyword: 'isNotEmpty',
-        message: 'должно иметь обязательное поле ' + key,
-        params: { keyword: 'isNotEmpty' }
-      }
-    ];
-
-    return typeof data === 'string' && data.trim() !== '';
-  }
-});
-
-ajv.addKeyword('maskedNumberLength', {
-  type: 'string',
-  errors: true,
-  validate: function validate(length, data) {
-    validate.errors = [
-      {
-        keyword: 'maskedNumberLength',
-        message: 'должно быть длинной ' + length + ' символов',
-        params: { keyword: 'maskedNumberLength' }
-      }
-    ];
-
-    const numsLength = data.replace(/\D/g, '').length;
-
-    return typeof data === 'string' && (numsLength === 0 || numsLength === length);
-  }
-});
-
-ajv.addKeyword('maskedNumberLengthStrict', {
-  type: 'string',
-  errors: true,
-  validate: function validate(length, data) {
-    validate.errors = [
-      {
-        keyword: 'maskedNumberLength',
-        message: 'должно быть длинной ' + length + ' символов',
-        params: { keyword: 'maskedNumberLength' }
-      }
-    ];
-
-    const numsLength = data.replace(/\D/g, '').length;
-
-    return numsLength === length;
-  }
-});
-
-ajv.addKeyword('inn', {
-  type: 'string',
-  errors: true,
-  validate: function validate(length, data) {
-    validate.errors = [
-      {
-        keyword: 'inn',
-        message: 'ИНН введен некорректно',
-        params: { keyword: 'inn' }
-      }
-    ];
-
-    return !data || checkInn(data);
-  }
-});
+registerAjvKeywords(ajv);
 
 export function createValidator(schema, additionalValidator) {
   const validator = ajv.compile(schema);
---
+++ src/utils/registerAjvKeywords.js
@@ -0,0 +1,104 @@
+import { checkInn } from './rules';
+
+export default (ajv) => {
+  ajv.addKeyword('isNotEmpty', {
+    type: 'string',
+    errors: true,
+    validate: function validate(schema, data, parent, key) {
+      validate.errors = [
+        {
+          keyword: 'isNotEmpty',
+          message: 'должно иметь обязательное поле ' + key,
+          params: { keyword: 'isNotEmpty' }
+        }
+      ];
+
+      return typeof data === 'string' && data.trim() !== '';
+    }
+  });
+
+  ajv.addKeyword('maskedNumberLength', {
+    type: 'string',
+    errors: true,
+    validate: function validate(length, data) {
+      validate.errors = [
+        {
+          keyword: 'maskedNumberLength',
+          message: 'должно быть длинной ' + length + ' символов',
+          params: { keyword: 'maskedNumberLength' }
+        }
+      ];
+
+      const numsLength = data.replace(/\D/g, '').length;
+
+      return typeof data === 'string' && (numsLength === 0 || numsLength === length);
+    }
+  });
+
+  ajv.addKeyword('maskedNumberLengthStrict', {
+    type: 'string',
+    errors: true,
+    validate: function validate(length, data) {
+      validate.errors = [
+        {
+          keyword: 'maskedNumberLength',
+          message: 'должно быть длинной ' + length + ' символов',
+          params: { keyword: 'maskedNumberLength' }
+        }
+      ];
+
+      const numsLength = data.replace(/\D/g, '').length;
+
+      return numsLength === length;
+    }
+  });
+
+  ajv.addKeyword('inn', {
+    type: 'string',
+    errors: true,
+    validate: function validate(length, data) {
+      validate.errors = [
+        {
+          keyword: 'inn',
+          message: 'ИНН введен некорректно',
+          params: { keyword: 'inn' }
+        }
+      ];
+
+      return !data || checkInn(data);
+    }
+  });
+
+  ajv.addKeyword('cyrillic', {
+    type: 'string',
+    errors: true,
+    validate: function validate(schema, data) {
+      validate.errors = [
+        {
+          keyword: 'cyrillic',
+          message: 'Для ввода доступна только кириллица.',
+          params: { keyword: 'cyrillic' }
+        }
+      ];
+      return !data || /^[0-9а-яё.,:!?";_'\-\s]+$/i.test(data); // кириллица, цифры и знаки препинания
+    }
+  });
+
+  ajv.addKeyword('latin', {
+    type: 'string',
+    errors: true,
+    validate: function validate(schema, data) {
+      validate.errors = [
+        {
+          keyword: 'latin',
+          message: 'Для ввода доступна только латиница.',
+          params: { keyword: 'latin' }
+        }
+      ];
+
+      return !data || /^[0-9a-z.,:!?";_'\-\s]+$/i.test(data); // латиница, цифры и знаки препинания
+    }
+  });
+
+  return ajv;
+};
